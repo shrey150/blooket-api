b976b4a662ebb8cf9a2929e6fc59b61ec7e5a7a0
shrey150 <shrey150@yahoo.com>
2020-11-18 10:50:29 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Blooket API
+===
+
+Provides control over [Blooket](https://www.blooket.com)'s internal APIs, allowing you to modify scores, update profile pictures, and kick players.
+
+Made for educational purposes; use at your own risk.
---
+++ index.js
@@ -0,0 +1,57 @@
+const SET_VAL_URL = "https://www.blooket.com/api/firebase/setval";
+const KICK_URL = "https://www.blooket.com/api/firebase/client";
+
+let gameId = null;
+let name = null;
+
+$(() => {
+    pics.forEach(n => $("#animalPic").append(`<option value="${n}">${n}</option>`));
+});
+
+$("#joinForm").submit(e => {
+
+    e.preventDefault();
+
+    gameId = $("#gameId").val();
+    name = $("#name").val();
+
+    $("#setup").hide();
+    $("#controls").show();
+
+});
+
+$("#setValForm").submit(e => {
+
+    e.preventDefault();
+
+    const animalPic = $("#animalPic").val();
+    const newScore = $("#newScore").val();
+
+    $.ajax({
+        url: SET_VAL_URL,
+        type: "PUT",
+        data: {
+            path: `${gameId}/c/${name}`,
+            val: {
+                b: animalPic,
+                g: newScore
+            }
+        },
+        success: () => console.log("Set score successfully"),
+    });
+
+});
+
+$("#kickForm").submit(e => {
+
+    e.preventDefault();
+
+    const kickName = $("#kickName").val();
+
+    $.ajax({
+        url: `${KICK_URL}?id=${gameId}&name=${kickName}`,
+        type: "DELETE",
+        success: () => console.log("Kicked player successfully"),
+    });
+
+});
